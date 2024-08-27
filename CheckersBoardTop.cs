1212507dcefea8a5d871408962b56f58abe5687e
Jake Crowley <jacrowley@captechu.edu>
2024-08-27 00:04:07 -0400

    fix board positioning and add animation

--- CheckersBoardTop.cs
+++ CheckersBoardTop.cs
@@ -5,26 +5,29 @@ using System.Collections.Generic;
 public partial class CheckersBoardTop : Sprite2D
 {
 	string replay = "board:0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,0,2,0,2,0,2,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0|move:5,2,4,3|board:0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,0,2,0,2,0,0,0,2,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0";
-	
+	Sprite2D movePiece;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Sprite2D redPiece = this.GetNode<Sprite2D>("CheckerPieceRed");
 		Sprite2D blackPiece = this.GetNode<Sprite2D>("CheckerPieceBlack");
 
-		var prevBoard = replay.Split('|')[2].Substring(6).Split(",");
+		var prevBoard = replay.Split('|')[0].Substring(6).Split(",");
+		var move = replay.Split("|")[1].Substring(5).Split(",");
+		var nextBoard = replay.Split('|')[2].Substring(6).Split(",");
 
 		for (int y = 0; y < 8; y++)
 		{
 			for (int x = 0; x < 8; x++)
 			{
-				string val = prevBoard[y * 8 + (7 - x)];
+				string val = prevBoard[(7 - y) * 8 + x];
 				Sprite2D newPiece = null;
-				if (val.Equals("1"))
+				if (val.Equals("2"))
 				{
 					newPiece = (Sprite2D)blackPiece.Duplicate();
 				}
-				else if (val.Equals("2"))
+				else if (val.Equals("1"))
 				{
 					newPiece = (Sprite2D)redPiece.Duplicate();
 				}
@@ -34,8 +37,15 @@ public partial class CheckersBoardTop : Sprite2D
 					this.AddChild(newPiece);
 					newPiece.Position = new Vector2(redPiece.Position.X + (135 * x), redPiece.Position.Y + (135 * y));
 					newPiece.Visible = true;
-					newPiece.Name = (x+1) + "," + (y+1);
-					GD.Print(x + 1, ", ", y + 1, " : ", newPiece.Position);
+					newPiece.Name = x + "," + (7 - y);
+					GD.Print(newPiece.Name, " : ", newPiece.Position);
+					if (newPiece.Name.Equals(move[0] + "," + move[1]))
+					{
+						Vector2 newPos = new Vector2(redPiece.Position.X + (135 * int.Parse(move[2])), redPiece.Position.Y + (135 * (int.Parse(move[3])+1)));
+						Tween tween = newPiece.GetTree().CreateTween();
+						tween.TweenProperty(newPiece, "position", newPos, 1.0f).SetTrans(Tween.TransitionType.Sine);
+						movePiece = newPiece;
+					}
 				}
 			}
 		}
